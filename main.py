ed862b802880255e7d237a915b6fb1a3f5ee4096
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-05 15:25:24 +0600

    main.py

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ def open_img(img_path):
 
     return carplate_img
 
+def carplate_extract(image, carplate_haar_cascade):
+    carplate_rects = carplate_haar_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5) # can be configured
+
+    for x, y, w, h in carplate_rects:
+        carplate_img = image[y+10:y+h, x+15:x+w-20] # can be configured
+
+    return carplate_img
+
+def enlarge_img(image, scale_percent):
+    width = int(image.shape[1]*scale_percent/100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    plt.axis('off')
+    resized_image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+    return resized_image
